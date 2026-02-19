c83da34edc3e885ff7ac266a7f99697f2c694a43
Juan Rojas <jrojas710@unab.edu.co>
2026-02-19 12:21:49 -0500

    Layout con show, index y create

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tienda TechStore</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+    {{-- NAVBAR --}}
+    @include('layout.navbar')
+
+    <h1>🚀 TechStore Premium</h1>
+
+    <div class="grid">
+        @foreach($productos as $producto)
+            <div class="card">
+                <img src="{{ $producto['imagen'] }}">
+                <div class="card-body">
+                    <h3>{{ $producto['nombre'] }}</h3>
+                    <p class="price">$ {{ number_format($producto['precio']) }}</p>
+                    <p class="estado">{{ $producto['estado'] }}</p>
+                    <a href="#" class="btn">Ver Producto</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    {{-- FOOTER --}}
+    @include('layout.footer')
+
+</body>
+</html>
