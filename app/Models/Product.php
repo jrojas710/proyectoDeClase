6198a081de65345cf69e7dfbd2a0ab91eca434f8
CSU22-AINF <CSU22@academico.unab.ad>
2026-02-26 11:14:58 -0500

    Modelos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Product extends Model
 {
-    protected $table = 'categories';
+    protected $table = 'products';
 }
